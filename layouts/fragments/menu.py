a757a161a45b5ff76a3c787dc8941356a368e345
Felipe Boff Nunes <felipe.nunes@bairesdev.com>
2022-02-25 00:22:13 -0300

    Menu fragment backbone.

--- layouts/fragments/menu.py
+++ layouts/fragments/menu.py
@@ -1,10 +1,11 @@
 from dash import html, dcc
+import dash_bootstrap_components as dbc
 
-menu_fragment = html.Div([
-    html.H1('Arson Enterprises'),
-    dcc.Tabs(id='menu_fragment_tabs', value='menu_fragment_tab_1', children=[
+menu_fragment = dbc.Row([
+    dbc.Col(html.P('Arson Enterprises'), width=2),
+    dbc.Col(dcc.Tabs(id='menu_fragment_tabs', value='menu_fragment_tab_1', children=[
         dcc.Tab(label='Tab 1', value='menu_fragment_tab_1'),
         dcc.Tab(label='Tab 2', value='menu_fragment_tab_2'),
         dcc.Tab(label='Tab 3', value='menu_fragment_tab_3'),
-    ]),
-])
+    ]), width=10, style={'width': '100%'}),
+], style={'display': 'flex'})
