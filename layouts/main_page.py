4d7505474ba00af0dd3de0bccd49d47ac40c6db7
Felipe Boff Nunes <felipe.nunes@bairesdev.com>
2022-02-24 23:42:21 -0300

    SD material UI installed

--- layouts/main_page.py
+++ layouts/main_page.py
@@ -1,5 +1,7 @@
 from dash import html
+import sd_material_ui as mui
 
-main_page_layout = html.Div(children=[
-    html.Div(children="Arson Enterprises"),
-])
+main_page_layout = mui.Paper(html.Div(children=[
+        html.Div(children="Arson Enterprises"),
+    ])
+)
